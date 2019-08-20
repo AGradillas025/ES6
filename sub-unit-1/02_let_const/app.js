0c1e4dcd8774db85ef3e1ceac68a41c4b52b240c
Anthony Gradillas <AGradillas025@west-mec.org>
2019-08-19 18:18:29 -0700

    Initial Commit
    In this commit push I was taught about var, let, const, and multiple scopes.

---
+++ sub-unit-1/02_let_const/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var a = 'Test1';
+var b = 'Test2';
+
+console.log(`Logged: ${a}`);
+
+function testVar() {
+    let a = 30;
+    console.log(`Function Logged: ${a}`);
+
+    if (true) {
+        var a = 50;
+        console.log(`Blocked Logged: ${a}`);
+    }
+    console.log("here: "+a)
+}
+testVar();
+console.log(`END Logged: ${a}`);
